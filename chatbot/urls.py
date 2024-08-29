6fb01c052d0bff5dda81f6c7cf431629ab13713a
kajanav <vkajana@std.appsc.sab.ac.lk>
2024-08-29 17:32:02 +0530

    update

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.chatbot, name='chatbot'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('logout', views.logout, name='logout'),
+    
 ]
